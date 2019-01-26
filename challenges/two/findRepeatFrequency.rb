19805ed8c4cf9548807806b3472c3a224f6294fe
Ben Cawrse <bcawrse@gmail.com>
2019-01-26 16:30:05 -0500

    Fixed repeatFrequency so it loops the list

--- challenges/two/findRepeatFrequency.rb
+++ challenges/two/findRepeatFrequency.rb
@@ -1,31 +1,35 @@
 #!/usr/bin/env ruby
 
 file = ARGV.first
-frequency = 0
 lines = IO.readlines(file, chomp: true)
 quitLimit = 2;
-
+frequency = 0
 freqCount = Hash.new(0);
 freqCount[frequency] = 1;
+indx = 0;
 
-lines.each do |line|
-#    puts "First value of line: #{line[0,1]}"
+until freqCount[frequency] == quitLimit
+    line = lines[indx];
+
     if line[0,1] == "+"
         addVal = line[1, line.length - 1].to_i
         frequency += addVal 
-#        puts "Add value #{addVal}"
     elsif line[0,1] == "-"
         subVal = line[1, line.length - 1].to_i
         frequency -= subVal 
-#        puts "Remove value #{subVal}"
     end
     
-    freqCount[frequency] = freqCount[frequency] + 1;
-    puts "\t frequency: #{frequency}"
+    freqCount[frequency] += 1
+    puts "\t frequency: #{frequency} : freqCount: #{freqCount[frequency]}"
     if freqCount[frequency] == quitLimit
         puts "FREQUENCY at #{quitLimit} is #{frequency}"
     end
-    
+
+   indx += 1
+
+   if indx >= lines.length
+       indx = 0
+   end
 end
 
 puts "Final Value: #{frequency}"
