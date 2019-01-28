a26ae1306572c89c6a07d7576b968c249f59beb0
Ben Cawrse <bcawrse@gmail.com>
2019-01-27 23:50:49 -0500

    Solution for day 2! Looks through all possibilities instead of quitting at result

--- challenges/two/compareBoxIds.rb
+++ challenges/two/compareBoxIds.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+Match = Struct.new(:match, :index) do
+end
+
 file = ARGV.first
 lines = IO.readlines(file, chomp: true)
 matches = []
@@ -9,12 +12,14 @@ lines.each do |line|
 
     lines.each do |innerLine|
         puts "Comparing #{line} to #{innerLine}"
-        mismatch = 0;
+        mismatch = 0
+        differIndx = 0
 
         for i in 0..line.length do
 
             if line[i] != innerLine[i]
                 mismatch += 1
+                differIndx = i
             end
 
             if mismatch >= 2
@@ -23,10 +28,14 @@ lines.each do |line|
         end
 
         if mismatch == 1
-            matches.push(line)
+            matches.push(Match.new(line.to_s, differIndx))
         end
     end
 end
 
-puts "Matches found #{matches}"
+puts "\n\nMatches found #{matches}"
 
+for i in 0..matches.length - 1 do
+    matchResult = matches[i].match.slice! matches[i].index
+    puts "Match removed index: #{matches[i].index} : #{matches[i].match}"
+end
