fb710110762589c2272bf36d90d1fdc36a2932ba
Ben Cawrse <bcawrse@gmail.com>
2019-01-27 23:13:06 -0500

    Working solution without showing complete answer

--- challenges/two/compareBoxIds.rb
+++ challenges/two/compareBoxIds.rb
@@ -1,8 +1,5 @@
 #!/usr/bin/env ruby
 
-Match = Struct.new(:match, :index) do
-end
-
 file = ARGV.first
 lines = IO.readlines(file, chomp: true)
 matches = []
@@ -12,14 +9,12 @@ lines.each do |line|
 
     lines.each do |innerLine|
         puts "Comparing #{line} to #{innerLine}"
-        mismatch = 0
-        differIndx = 0
+        mismatch = 0;
 
         for i in 0..line.length do
 
             if line[i] != innerLine[i]
                 mismatch += 1
-                differIndx = i
             end
 
             if mismatch >= 2
@@ -28,13 +23,10 @@ lines.each do |line|
         end
 
         if mismatch == 1
-            matches.push(new Match(line, differIndx))
+            matches.push(line)
         end
     end
 end
 
 puts "Matches found #{matches}"
 
-for i in 0..matches.length do
-   puts "Match without differing letter at index #{matches.index} : #{matches.match.slice! match.index}"
-end
